c6afe292da2ba7b457f4857a087ff47e747dba53
nimzyow <n_soufiani@hotmail.com>
2020-05-11 22:39:48 +0100

    removed bootstrap and added materialize

--- nima-profile/src/App.js
+++ nima-profile/src/App.js
@@ -1,22 +1,35 @@
-import React, { useState } from "react";
-import { Modal, Button } from "react-bootstrap";
+import React, { useEffect } from "react";
+
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css/dist/js/materialize.min.js";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    //initialises Materialise JS
+    M.AutoInit();
+  }, []);
+
   return (
-    <div
-      style={{
-        border: "1px solid purple",
-        height: "100%",
-        width: "100vw",
-        position: "absolute",
-      }}
-    >
-      <canvas
-        width="2166"
-        height="1542"
-        style={{ width: "1083px", height: "771px" }}
-      />
-      Hello World!ss sdasd
+    <div style={{ display: "flex", flexDirection: "row" }}>
+      <a className="waves-effect waves-light btn modal-trigger" href="#modal1">
+        Modal
+      </a>
+
+      <div id="modal1" className="modal">
+        <div className="modal-content">
+          <h4>Modal Header</h4>
+          <p>A bunch of text</p>
+        </div>
+        <div className="modal-footer">
+          <a
+            href="#!"
+            className="modal-close waves-effect waves-green btn-flat"
+          >
+            Agree
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
