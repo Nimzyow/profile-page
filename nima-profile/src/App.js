83915f1f00363c098358dccfe004060b14c0d87a
nimzyow <n_soufiani@hotmail.com>
2020-05-11 21:49:22 +0100

    added canvas

--- nima-profile/src/App.js
+++ nima-profile/src/App.js
@@ -3,8 +3,20 @@ import { Modal, Button } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <div className="container">Hello World!</div>
+    <div
+      style={{
+        border: "1px solid purple",
+        height: "100%",
+        width: "100vw",
+        position: "absolute",
+      }}
+    >
+      <canvas
+        width="2166"
+        height="1542"
+        style={{ width: "1083px", height: "771px" }}
+      />
+      Hello World!ss sdasd
     </div>
   );
 }
