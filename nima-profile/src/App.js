5bccd86857589709beac3ddca51b79ed2fe385ce
nimzyow <n_soufiani@hotmail.com>
2020-05-11 22:52:50 +0100

    created navbar

--- nima-profile/src/App.js
+++ nima-profile/src/App.js
@@ -11,24 +11,70 @@ function App() {
   }, []);
 
   return (
-    <div style={{ display: "flex", flexDirection: "row" }}>
-      <a className="waves-effect waves-light btn modal-trigger" href="#modal1">
-        Modal
-      </a>
-
-      <div id="modal1" className="modal">
-        <div className="modal-content">
-          <h4>Modal Header</h4>
-          <p>A bunch of text</p>
-        </div>
-        <div className="modal-footer">
-          <a
-            href="#!"
-            className="modal-close waves-effect waves-green btn-flat"
-          >
-            Agree
+    <div>
+      <nav className="nav-extended">
+        <div className="nav-wrapper">
+          <a href="#" className="brand-logo">
+            Logo
           </a>
+          <a href="#" data-target="mobile-demo" className="sidenav-trigger">
+            <i className="material-icons">menu</i>
+          </a>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li>
+              <a href="sass.html">Sass</a>
+            </li>
+            <li>
+              <a href="badges.html">Components</a>
+            </li>
+            <li>
+              <a href="collapsible.html">JavaScript</a>
+            </li>
+          </ul>
         </div>
+        <div className="nav-content">
+          <ul className="tabs tabs-transparent">
+            <li className="tab">
+              <a href="#test1">Test 1</a>
+            </li>
+            <li className="tab">
+              <a className="active" href="#test2">
+                Test 2
+              </a>
+            </li>
+            <li className="tab disabled">
+              <a href="#test3">Disabled Tab</a>
+            </li>
+            <li className="tab">
+              <a href="#test4">Test 4</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <ul className="sidenav" id="mobile-demo">
+        <li>
+          <a href="sass.html">Sass</a>
+        </li>
+        <li>
+          <a href="badges.html">Components</a>
+        </li>
+        <li>
+          <a href="collapsible.html">JavaScript</a>
+        </li>
+      </ul>
+
+      <div id="test1" className="col s12">
+        Test 1
+      </div>
+      <div id="test2" className="col s12">
+        Test 2
+      </div>
+      <div id="test3" className="col s12">
+        Test 3
+      </div>
+      <div id="test4" className="col s12">
+        Test 4
       </div>
     </div>
   );
