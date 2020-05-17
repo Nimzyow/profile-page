658cbba9eee43636d628272e7f3d7c9f71a4feb3
Nimzyow <n_soufiani@hotmail.com>
2020-05-17 19:02:12 +0100

    Created background animation on landing page

---
+++ nima-profile/src/components/pages/landingPage/LandingPage.js
@@ -0,0 +1,22 @@
+import React, { Fragment } from "react";
+import "./LandingPage.scss";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <div className="background-container">
+        <img
+          src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1231630/moon2.png"
+          alt=""
+        />
+        <div className="stars"></div>
+        <div className="twinkling"></div>
+        <div className="clouds"></div>
+      </div>
+      <h1 className="welcome">Welcome to my Developer page</h1>
+      <h2 className="subWelcome">Click here to learn more</h2>
+    </div>
+  );
+};
+
+export default LandingPage;
