0e6a9a6d7bba8b9afe42883267a4f04e6defaf5f
Nimzyow <n_soufiani@hotmail.com>
2020-05-31 13:17:04 +0100

    Navbar now sticks when scrolling to specific point

--- nima-profile/src/App.js
+++ nima-profile/src/App.js
@@ -13,6 +13,23 @@ function App() {
     M.AutoInit();
   }, []);
 
+  useEffect(() => {
+    window.onscroll = function () {
+      myFunction();
+    };
+
+    var navbar = document.getElementById("navBar");
+    var sticky = navbar.offsetTop;
+
+    function myFunction() {
+      if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky");
+      } else {
+        navbar.classList.remove("sticky");
+      }
+    }
+  }, []);
+
   return (
     <div style={{ overflowY: "scroll", overflowX: "hidden" }}>
       <LandingPage />
--- nima-profile/src/components/layout/navbar/NavBar.js
+++ nima-profile/src/components/layout/navbar/NavBar.js
@@ -24,24 +24,6 @@ const NavBar = () => {
             </li>
           </ul>
         </div>
-        <div className="nav-content">
-          <ul className="tabs tabs-transparent">
-            <li className="tab">
-              <a href="#test1">Test 1</a>
-            </li>
-            <li className="tab">
-              <a className="active" href="#test2">
-                Test 2
-              </a>
-            </li>
-            <li className="tab disabled">
-              <a href="#test3">Disabled Tab</a>
-            </li>
-            <li className="tab">
-              <a href="#test4">Test 4</a>
-            </li>
-          </ul>
-        </div>
       </nav>
 
       <ul className="sidenav" id="mobile-demo">
@@ -55,19 +37,6 @@ const NavBar = () => {
           <a href="collapsible.html">JavaScript</a>
         </li>
       </ul>
-
-      <div id="test1" className="col s12">
-        Test 1
-      </div>
-      <div id="test2" className="col s12">
-        Test 2
-      </div>
-      <div id="test3" className="col s12">
-        Test 3
-      </div>
-      <div id="test4" className="col s12">
-        Test 4
-      </div>
     </div>
   );
 };
