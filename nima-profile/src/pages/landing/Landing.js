06eef8b023f8d81e132648f717fcbfb5a9de0486
Nimzyow <n_soufiani@hotmail.com>
2020-07-31 14:30:19 +0100

    modification for aws

--- nima-profile/src/pages/landing/Landing.js
+++ nima-profile/src/pages/landing/Landing.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
 
-import underwaterImage from "../../assets/isometricunderwater.png";
+//import underwaterImage from "../../assets/isometricunderwater.png";
 import "./LandingPage.scss";
 
 export const Landing = () => {
